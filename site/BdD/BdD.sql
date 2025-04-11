51e4fb8969c4f8de9148db3aa0cee0ad0c10f014
MeXam_ <maxence.pascalis84@gmail.com>
2025-04-11 11:57:53 +0000

    deleted:    site/BdD/BdD.sql renamed:    site/DB php-1744372555.sql -> site/DB.sql

--- site/BdD/BdD.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE music (
-    ID_music INTEGER PRIMARY KEY AUTO_INCREMENT,
-    Nom_music TEXT NOT NULL,
-    Artiste TEXT NOT NULL,
-    Prix TEXT NOT NULL,
-    Style TEXT,
-    Music_description TEXT
-);
