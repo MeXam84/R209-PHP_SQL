51ab034133fe762d264a7efbaf9185609d8528bb
Epilow <benjaminchaumet2@gmail.com>
2025-04-11 14:09:13 +0200

    new file:   site/main.css modified:   site/main.php

--- site/main.php
+++ site/main.php
@@ -1,9 +1,10 @@
 <!DOCTYPE html>
 <html lang="fr">
+<link ref="stylesheet" href="main.css">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
+    <div id="header"><title>Index</title> </div>
 </head>
 <body>
     <p>Test</p>
