338c1fa7e0bcafbb174d9f612cea1743c0c05619
Epilow <benjaminchaumet2@gmail.com>
2025-04-11 15:28:01 +0200

    modified:   site/main.css 	modified:   site/main.php

--- site/main.php
+++ site/main.php
@@ -2,15 +2,17 @@
 <html lang="fr">
 <link rel="stylesheet" href="main.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<head>
+</head>
+<body> 
 <header class="navbar">
     <div class="nav-left">
       <i class="fa fa-home"></i>
     </div>
     <div class="nav-center">
-      <img src="logo.png" alt="Logo" class="logo">
+      <img src="logo.png" alt="Logo" class="logo">  
         </div>
     <div class="nav-right">
       <i class="fa fa-search"></i>
@@ -18,8 +20,16 @@
       <i class="fa fa-user-circle "></i>
     </div>
   </header>
-</head>
-<body> 
-    <p>Test</p>
+
+  <div class="container">
+    <div class="main">
+    <h1>
+      This is the title
+    </h1>
+    <p>This is a paragraph paragraphparagraphpararrrrrgrapaaaaaaaaaah.</p>
+  </div> 
+</div>
+
+
 </body>
 </html>
