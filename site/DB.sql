c287c1fe6a53cffac9027a53718b0644307f16b1
MeXam84 <maxence.pascalis84@gmail.com>
2025-04-11 16:03:30 +0200

    DB

--- site/DB.sql
+++ site/DB.sql
@@ -1,29 +1,42 @@
 CREATE TABLE IF NOT EXISTS `Music` (
-	`Id_music` integer primary key NOT NULL UNIQUE,
+	`Id_music` integer primary key NOT NULL UNIQUE IDENTITY,
 	`Nom_music` TEXT NOT NULL,
-	`Album` TEXT NOT NULL,
-	`Artiste` TEXT NOT NULL,
+	`Id_album` INTEGER NOT NULL,
+	`Id_artiste` INTEGER NOT NULL,
 	`Prix` TEXT NOT NULL,
 	`Style` TEXT NOT NULL,
 	`Description` TEXT NOT NULL,
-FOREIGN KEY(`Album`) REFERENCES `Album`(`Nom_album`),
-FOREIGN KEY(`Artiste`) REFERENCES `Artiste`(`Nom_artiste`)
+FOREIGN KEY(`Id_music`) REFERENCES `Panier`(`Id_music`),
+FOREIGN KEY(`Id_artiste`) REFERENCES `Album`(`Id_artiste`)
 );
 CREATE TABLE IF NOT EXISTS `Artiste` (
-	`Id_artiste` integer primary key NOT NULL UNIQUE,
+	`Id_artiste` integer primary key NOT NULL UNIQUE IDENTITY,
 	`Nom_artiste` TEXT NOT NULL,
-FOREIGN KEY(`Nom_artiste`) REFERENCES `Album`(`Artiste`)
+FOREIGN KEY(`Id_artiste`) REFERENCES `Music`(`Id_artiste`)
 );
 CREATE TABLE IF NOT EXISTS `Album` (
-	`Id_album` integer primary key NOT NULL UNIQUE,
+	`Id_album` integer primary key NOT NULL UNIQUE IDENTITY,
 	`Nom_album` TEXT NOT NULL,
-	`Artiste` TEXT NOT NULL,
-	`Date_sortie` REAL NOT NULL,
-	`Description` TEXT NOT NULL
+	`Id_artiste` INTEGER NOT NULL,
+	`Date_sortie` TEXT NOT NULL,
+	`Description` TEXT NOT NULL,
+FOREIGN KEY(`Id_album`) REFERENCES `Music`(`Id_album`)
 );
 CREATE TABLE IF NOT EXISTS `Users` (
-	`Id_users` integer primary key NOT NULL UNIQUE,
+	`Id_users` integer primary key NOT NULL UNIQUE IDENTITY,
 	`Nom_users` TEXT NOT NULL,
 	`Password` TEXT NOT NULL,
-	`Perm` TEXT NOT NULL
+	`Perm` TEXT NOT NULL,
+FOREIGN KEY(`Id_users`) REFERENCES `Panier`(`Id_users`)
+);
+CREATE TABLE IF NOT EXISTS `Panier` (
+	`Id_users` INTEGER NOT NULL,
+	`Id_music` INTEGER NOT NULL,
+	`QTE` INTEGER NOT NULL,
+FOREIGN KEY(`Id_users`) REFERENCES `Commande`(`Id_users`)
+);
+CREATE TABLE IF NOT EXISTS `Commande` (
+	`Id_commande` integer primary key NOT NULL UNIQUE IDENTITY,
+	`Id_users` INTEGER NOT NULL,
+	`Date_commande` REAL NOT NULL
 );
