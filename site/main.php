bb8f8d5b789e1c3f8797415b7c3ce9160927d2de
Epilow <benjaminchaumet2@gmail.com>
2025-04-11 14:52:44 +0200

    modified:   site/main.css 	modified:   site/main.php

--- site/main.php
+++ site/main.php
@@ -2,12 +2,24 @@
 <html lang="fr">
 <link rel="stylesheet" href="main.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <div id="header"> <img src="logo.png" width= 30% class="center"> </div>
+<header class="navbar">
+    <div class="nav-left">
+      <i class="fa fa-home"></i>
+    </div>
+    <div class="nav-center">
+      <img src="logo.png" alt="Logo" class="logo">
+        </div>
+    <div class="nav-right">
+      <i class="fa fa-search"></i>
+      <i class="fa fa-shopping-cart"></i>
+      <i class="fa fa-user-circle "></i>
+    </div>
+  </header>
 </head>
-<body>
+<body> 
     <p>Test</p>
 </body>
 </html>
