257c53cf93dbc49256fda709e99e440454b4e716
Epilow <benjaminchaumet2@gmail.com>
2025-04-16 11:48:15 +0200

    modified:   site/dashboard.php 	new file:   site/logout.php

--- site/dashboard.php
+++ site/dashboard.php
@@ -7,13 +7,52 @@ if (!isset($_SESSION['username'])) {
 ?>
 
 <!DOCTYPE html>
-<html>
+<html lang="fr">
+<link rel="stylesheet" href="main.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
     <title>Tableau de bord</title>
 </head>
 <body>
-    <h1>Bienvenue, <?php echo htmlspecialchars($_SESSION['username']); ?> !</h1>
+<header class="navbar">
+    <div class="nav-left">
+    <a href="main.php" style="color: #242124;">
+      <i class="fa fa-home"></i>
+</a> 
+    </div>
+    <div class="nav-center">
+      <a href="main.php" class="logo-link">
+      <img src="logo.png" alt="Logo" class="logo">
+  </a>  
+        </div>
+    <div class="nav-right"> 
+      <i class="fa fa-search" style="color: #242124;"></i> 
+      <a href="panier.php" style="color: #242124;">
+      <i class="fa fa-shopping-cart"></i>
+</a>
+      <a href="login.php" style="color: #242124;">
+  <i class="fa fa-user-circle"></i>
+</a>
+    </div>
+  </header>
+  <div class="container">
+    <div class="main">
+  <h1>Bienvenue, <?php echo htmlspecialchars($_SESSION['username']); ?> !</h1>
     <p>Vous êtes connecté.</p>
     <a href="logout.php">Se déconnecter</a>
+      
+    </div>
+  </div>
+
+  <div class="rect-footer">
+    <footer>
+      <p>© 2023 Mon Site Web. Tous droits réservés.</p>
+    </footer>
+  </div>
+
+
 </body>
 </html>
