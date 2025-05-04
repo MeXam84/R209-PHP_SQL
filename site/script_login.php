7332e4b831db8d6626ac00541dcc9c40f4f98a56
MeXam84 <maxence.pascalis84@gmail.com>
2025-05-05 00:06:52 +0200

    ajout du système de connexion via une DB

--- site/script_login.php
+++ site/script_login.php
@@ -1,18 +1,22 @@
 <?php
 session_start();
 
-// Simule une "base de données"
-$users = [
-    'admin' => 'admin',
-    'user' => 'admin'
-];
+// Connexion à la base de données SQLite
+$pdo = new PDO('sqlite:SQL.db');
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 // Récupère les données du formulaire
 $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 
+// Vérification dans la base
+$stmt = $pdo->prepare('SELECT * FROM Users WHERE Nom_users = ?');
+$stmt->execute([$username]);
+$user = $stmt->fetch();
+
+
 // Vérifie si les identifiants sont valides
-if (isset($users[$username]) && $users[$username] === $password) {
+if ($user && $password === $user['Password']) {
     $_SESSION['username'] = $username;
     header('Location: dashboard.php');
     exit;
